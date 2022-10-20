b9d0c57810f9971214efdbb42b8b2297673dc585
Lucián Švancár <svancarlucian@gmail.com>
2022-10-20 08:39:37 +0200

    The first and last letter from a string with if and substring. add funcion returning without first and last letter in string

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,19 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+
+            if (original.Length >= 2)
+            {
+                string result = original.Substring(1, original.Length - 2);
+
+                return result;
+            }
+
+            else
+            {
+                return original;
+            }
+
         }
     }
 }
